6027272cc5b066171fed051267ae6226da779e5b
Shafi001s <shafiahmed001s@gmail.com>
2025-12-19 17:31:17 +0000

    Implemented TensorFlow

--- Softmax_TF/sofmax.py
+++ Softmax_TF/sofmax.py
@@ -14,7 +14,7 @@ import logging
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 tf.autograph.set_verbosity(0)
 
-
+#Softmax function implementation
 def my_softmax(z):
     ez = np.exp(z)
     sm = ez/np.sum(ez)
@@ -22,3 +22,29 @@ def my_softmax(z):
 
 plt.close("all")
 plt_softmax(my_softmax)
+
+#Exmaple data set
+centers = [[-5,2],[-2,-2],[1,2],[5,-2]]
+X_train, y_train = make_blobs(n_samples= 2000, centers = centers, cluster_std = 1.0, random_state=30) #make_blobs generates isotropic Gaussian blobs for clustering.
+
+model =Sequential(
+    [
+        Dense(25, activation = 'relu'),
+        Dense(15, activation = 'relu'),
+        Dense(10, activation = 'softmax')
+    ]
+)
+
+model.compile(
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(),
+    optimizer = tf.keras.optimizers.Adam(0.001),
+)
+
+model.fit(
+    X_train, y_train,
+    epochs = 10
+)
+
+P_nonpreferred = model.predict(X_train)
+print(P_nonpreferred[:2])
+print("largest value", np.max(P_nonpreferred), "smallest value", np.min(P_nonpreferred))
